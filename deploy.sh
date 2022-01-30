dd5f948ea21afa81b55c1a64cd79a31075bae511
panjianning <panjianning@huya.com>
2022-01-30 22:09:49 +0800

    use plantuml https link

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #npm run build
-git add . && git commit -m "$1" && git push origin master
+#git add . && git commit -m "$1" && git push origin master
 cp dist ../puml-dist -rf && git add . && git commit -m "$1" && git push origin gh-pages
 
