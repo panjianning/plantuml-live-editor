3458231cb6889aafeb84416cbb3c5dab14b6b4cc
panjianning <panjianning@huya.com>
2022-01-31 21:02:53 +0800

    add google analytics

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#npm run build;
+npm run build;
 git add . && git commit -m "$1" && git push origin master;
-cp dist/* ../puml-dist -rf && cd ../puml-dist && git add . && git commit -m "$1" && git push origin gh-pages && cd ../puml;
+cp dist/* ../puml-dist -Rf && cd ../puml-dist && git add . && git commit -m "$1" && git push origin gh-pages && cd ../puml || exit ;
 
